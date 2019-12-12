12e9bab06d3ecb06886e124940b58b6b164e5259
liuzhenpeng <liuzhenpeng@jdd.com>
2019-12-12 11:35:06 +0800

    feat(*): design pattern chain

---
+++ src/main/java/com/ppg/man/algorithm/linkedlist/Data.java
@@ -0,0 +1,15 @@
+package com.ppg.man.algorithm.linkedlist;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Data {
+
+    private Data prev;
+
+    private String value;
+
+    private Data next;
+}
--- src/main/java/com/ppg/man/hessian/ProviderService.java
+++
@@ -1,6 +0,0 @@
-package com.ppg.man.hessian;
-
-public interface ProviderService {
-
-    void test();
-}
